026d0ee1f129ff3136668552549092ff8a55ce9c
catherine-hhs <58843684+catherine-hhs@users.noreply.github.com>
2021-12-27 11:39:54 +0800

    Add files via upload

---
+++ src/components/resault.vue
@@ -0,0 +1,97 @@
+<template>
+<v-app id="inspire">
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+
+        <v-btn
+          @click="tomember"
+        >
+          member
+        </v-btn>
+
+        <v-spacer></v-spacer>
+        <p >疾病名：</p>
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      疾病 {{ n }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <div v-for="data in jsonData" :key="data.title">{{ data.title }}</div>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+// import JsonViewer from 'vue-json-viewer'
+import test from '../data/test.json'
+
+export default {
+  name: 'App',
+
+  components: {
+    //JsonViewer
+  },
+
+  data(){
+    return {
+      jsonData:test
+    }
+  },
+};
+</script>
+
+<style>
+  .q-search{
+    margin-top: 5vh;
+    margin-left: 20vw;
+  }
+  .search{
+    margin-top: 5vh;
+    margin-left: 3vh;
+  }
+</style>
---
+++ src/components/search.vue
@@ -0,0 +1,77 @@
+<template>
+<v-app id="inspire">
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <v-btn
+          @click="tomember"
+        >
+          member
+        </v-btn>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="auto" class="q-search">
+            <p >疾病名：</p>
+          </v-col>
+          <v-col cols="4" class="search">
+            <img height="100px" width="130px" class="search-tu" src="../assets/search.png">
+            <v-text-field
+              label="请输入search"
+              dense
+              persistent-hint
+              rounded
+              solo-inverted
+              background-color="grey"
+              append-outer-icon="mdi-arrow-right-box"
+              @click:append-outer="toresault"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  methods:{
+    tosearch:function(){
+      this.$router.push({
+        path:'/search'
+        });
+    },
+    toresault:function(){
+      this.$router.push({
+        path:'/resault'
+        });
+    },
+    tomember:function(){
+      this.$router.push({
+        path:'/member'
+      })
+    }
+  }
+}
+</script>
+
+<style>
+  .q-search{
+    margin-top:36vh;
+    margin-left: 20vw;
+  }
+  .search-tu{
+    margin-left: 5vw;
+  }
+  .search{
+    margin-top:20vh;
+    margin-left: 3vh;
+  }
+</style>
